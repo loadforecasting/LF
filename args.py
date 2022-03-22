210f35aefe80b99d0d3464b49fb309105c5c3ea4
loadforecasting <99839693+loadforecasting@users.noreply.github.com>
2022-03-22 14:33:09 +0100

    added_args

---
+++ args.py
@@ -0,0 +1,5 @@
+window_size=96#to capture both daily and monthly seasonality
+batch_size=128# make sure the len of data is a multiply
+n_horizon=1# forecast one step into the future
+shuffle_buffer_size=1000
+shift=1
